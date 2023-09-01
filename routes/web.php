8db2e19297130f48de136b291db57e6bc682b81b
Franjisco-1 <hezobysg@gmail.com>
2023-09-01 15:16:30 +0700

    menutup akses register user

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(['register'=> false]);
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
